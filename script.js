7e7fe6cf6b9d7261a2d3852a2a6eada7b1d0a8ca
Julia <julvas1994@gmail.com>
2022-12-24 11:04:14 +0300

    clean code

--- script.js
+++ script.js
@@ -51,36 +51,3 @@ function getTotalProgressByRecurtion(data) {
 }
 const result = getTotalProgressByRecurtion(students);
 console.log(result[0]/result[1]);
-
-let studs = {
-    js: [{
-        name: 'John',
-        progress: 100
-    }, {
-        name: 'Ivan',
-        progress: 60
-    }],
-
-    html: {
-        basic: [{
-            name: 'Peter',
-            progress: 20
-        }, {
-            name: 'Ann',
-            progress: 18
-        }],
-
-        pro: [{
-            name: 'Sam',
-            progress: 10
-        }],
-
-        semi: {
-            students: [{
-                name: 'Test',
-                progress: 100
-            }]
-        }
-    }
-};
-
